816f003d6ba159e1a5068ee99c64315e0ed92c04
ricardocollado <ricardocolladorothschild@gmail.com>
2023-12-03 23:57:21 -0400

    possible find solution

--- aDev_02/solution.js
+++ aDev_02/solution.js
@@ -2,11 +2,11 @@ const fs = require('fs').promises;
 const goal_Red = 12;
 const goal_Green = 13;
 const goal_Blue = 14;
-let color_Objects = {green:0,blue:0,red:0}
+let color_Objects = {'green':0,'blue':0,'red':0}
 let games = [];
 async function convertDataToArray() {
     try {
-      const data = await fs.readFile('./testdata', 'utf8');
+      const data = await fs.readFile('./input.txt', 'utf8');
       const lines = data.split('\n');
       const filteredLines = lines.filter(line => line.trim() !== '');
       
@@ -19,7 +19,9 @@ async function convertDataToArray() {
 
   function returnDigits(stringDigits){
 
-    const regex = /\d+/g;
+    
+    //const regex = /\d+/g;
+    const regex = /([0-9]+)/g;
     return stringDigits.match(regex);
 }
 
@@ -34,57 +36,70 @@ function returnColor(stringColor){
     
   let arreglo = await convertDataToArray();
   let arrayMultidimensional = [];
-  console.log(arreglo[0]);
+  
     for(let i = 0; i < arreglo.length; i ++ ){
         arrayMultidimensional[i] = [];
         let subSet = arreglo[i].split(';');
+        
             for(let j = 0; j < subSet.length; j++){
+                
                 arrayMultidimensional[i].push(subSet[j]);
                 
             }
-            arrayMultidimensional[i][0] = arrayMultidimensional[i][0].replace(':',",");
+            arrayMultidimensional[i][0] = arrayMultidimensional[i][0].replace(': ',",");
 
             
     }
-        for(let i = 0; i < arrayMultidimensional.length-1; i++){
+    
+        for(let i = 0; i < arrayMultidimensional.length; i++){
             let currentGame;
             for(let j = 0; j < arrayMultidimensional[i].length;j++){
                 const subSet = arrayMultidimensional[i][j].split(',');
+                console.log(subSet);
                 if(j===0){
                     currentGame= Number(returnDigits(subSet[0]));
                     //games.push(temp); -> this must be move to the part of code where we confirm current game applies.
                 }
-                    subSet.forEach((set, indx)=>{
+                    subSet.forEach((set)=>{
 
                             
                             const number =Number(returnDigits(set));
                             const color = returnColor(set);
-                                if(color!=='Game'){
-                                    color_Objects[color] +=number;
-                                }
-                            
-                        
+
+                                    if(color==='red'){
+                                        color_Objects['red'] +=number;
+
+                                    }else if(color==='blue'){
+                                        color_Objects['blue'] +=number;
+                                        
+                                    }else if(color==='green'){
+                                        color_Objects['green'] +=number;
+                                    }
                         
                     });
+                 
 
-
+                    
             }
-                console.log(currentGame);
-                console.log(color_Objects);
-                if((color_Objects.blue <= goal_Blue) && (color_Objects.red <= goal_Red) && (color_Objects.green <= goal_Green)){
-                    console.log('get into condition');
-                        games.push(currentGame);
-                }
+            console.log(currentGame);
+            console.log(color_Objects);
+            if((color_Objects.blue <= goal_Blue) && (color_Objects.red <= goal_Red) && (color_Objects.green <= goal_Green)){
+                console.log('get into condition');
+                    games.push(currentGame);
+            }
+                    
 
 
+
+                currentGame = 0;
             color_Objects.blue = 0;
             color_Objects.green = 0;
             color_Objects.red = 0;
         }
-    console.log(arrayMultidimensional);
+    /*console.log(arrayMultidimensional);*/
     console.log(games);
     const sum = games.reduce((accumulator, newNum)=>{
-        return accumulator +newNum;
+        return accumulator + newNum;
       },0);
       console.log(sum);
   }
